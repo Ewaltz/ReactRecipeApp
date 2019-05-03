0507c844f095930410c53a887b9c1c66692bdeee
WenJun Pan <kurowpan@gmail.com>
2019-05-03 14:52:18 -0700

    Clear warnings

--- src/App.js
+++ src/App.js
@@ -10,22 +10,26 @@ const App = ()=>{
   const [search, setSearch] = useState('');
   const [query, setQuery] = useState('chicken');
 
-
+//useEffect renders on load, then renders info in array
+//await used for waiting information returned from api
   useEffect(()=>{
+    const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const data = await response.json();
+      setRecipes(data.hits);
+      console.log(data.hits);
+    };
     getRecipes();
   }, [query]);
 
-  const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data = await response.json();
-    setRecipes(data.hits);
-    console.log(data.hits);
-  }
 
+  //update search when info is put into searchbar
   const updateSearch = e => {
     setSearch(e.target.value);
   }
   
+  //Search only when search button is clicked to prevent
+  //constant query to API
   const getSearch = e => {
     e.preventDefault();
     setQuery(search);
@@ -44,7 +48,7 @@ const App = ()=>{
             <Recipe 
             key = {recipe.recipe.label}
             title={recipe.recipe.label} 
-            calories={recipe.recipe.calories} 
+            calories={recipe.recipe.calories.toFixed(2)} 
             image = {recipe.recipe.image}
             ingredients = {recipe.recipe.ingredients}/>
         ))}
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import style from './recipe.module.css';
+
+//Recipe object for render; styalized with html
 const Recipe = ({title, calories, image, ingredients})=>{
     return(
         <div className={style.recipe}>
             <h1>{title}</h1>
+            <p>Recipe</p>
             <ol>
-                {ingredients.map(ingredient =>(
-                    <li>{ingredient.text}</li>
-                )
-                    
-                )}
+                {ingredients.map((ingredient,i )=>(
+                    <li className="ingredient-item" key = {i}>{ingredient.text}</li>
+                ))}
             </ol>
-            <p>{calories}</p>
+            <p><b>Calories: </b> {calories}</p>
             <img className={style.image} src={image} alt=""/>
         </div>
     );
